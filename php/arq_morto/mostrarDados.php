60f0d0b91c6da7be4ad1d639834a737df699de6d
Emanuel G <emanuelgdesouza@gmail.com>
2016-10-07 10:20:12 -0300

    Implementei ajax no formulario de pesquisa de livros da secao arquivo morto

---
+++ php/arq_morto/mostrarDados.php
@@ -0,0 +1,29 @@
+<?php
+// Arquivo responsável por adicionar os alunos no banco de dados
+
+//Requerindo os arquivos
+require "../funcoes.php";
+
+// Recuperando as variáveis do método POST e declarando as variáveis
+$idlivro = isset($_GET["query"])?$_GET["query"]:"";
+
+$resultados  = ["idlivro_arq", "titulo", "autor", "editora", "gaveta", "livro", "estoque"];
+
+// Chamando a função de conexão
+$con = conectaDB();
+if($con){}else {echo "<p> Não houve conexão <br> </p>";	die(mysqli_error($con));}
+
+// Mostrando os dados escolhidos
+echo "<p> Os dados do livros são: </p>";
+$q    = "SELECT * FROM arquivo_morto WHERE idlivro_arq = $idlivro";
+$sel  = executaQuery($con, $q);
+$data = mysqli_fetch_all($sel, MYSQLI_ASSOC);
+for($x = 0; $x < count($resultados); $x++){
+    $a = $data[0][$resultados[$x]];
+    $b = ucwords($resultados[$x]);
+    echo "<p> $b : <span> $a </span></p>";
+}
+
+// Fechando a conexão
+fechaDB($con);
+?>
--- php/arq_morto/pesquisa_livro.php
+++ php/arq_morto/pesquisa_livro.php
@@ -1,63 +1,52 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8" />
-    <title> Dados Adicionados ao cadastro </title>
-    <link rel="stylesheet" href="../css/style_php.css" />
-</head>
-<body>
-    <div class="interface">
-       <button class="bt-voltar" onclick="window.location.href = '../form_pesquisar_arq_morto.html'"> Voltar </button>
-        <div class="result">
-			<form action="mostrarDados.php" method="get">
-            <?php
+<?php
 
-            //Requerindo os arquivos
-            require "funcoes.php";
+//Requerindo os arquivos
+require "../funcoes.php";
 
-            // Recuperando as variáveis do método POST
-            $titulo   = isset($_POST["titulo"])?$_POST["titulo"]:"";
-            $titulo   = strtoupper($titulo);
-			$autor    = isset($_POST["autor"])?$_POST["autor"]:"";
-            $autor    = limparNome($autor);
+// Chamando a função de conexão
+$con = conectaDB();
+if($con){}else {echo "<p> Não houve conexão <br> </p>";	die(mysqli_error($con));}
 
-            // Chamando a função de conexão
-            $con = conectaDB();
-            if($con){}else {echo "<p> Não houve conexão <br> </p>";	die(mysqli_error($con));}
-            
-			// Primeiro, preciso saber se o usuário está requisitando um titulo ou autor
-			if($titulo != ""){ // caso seja título
-				$q   = "SELECT titulo, autor FROM arquivo_morto WHERE titulo LIKE '%$titulo%'";
-				$sel = executaQuery($con, $q);
-				$c   = mysqli_num_rows($sel);
-				if($c != 0){
-					echo "<p>Esta solicitação retornou em " . $c . " registro(s)</p>";
-					$data = mysqli_fetch_all($sel, MYSQLI_ASSOC);
-					for($x = 0; $x < count($data); $x++){
-						$valor = $data[0]['titulo'] . " de " . $data[0]['autor'];
-						echo "<button type='submit' name='esc' value='$valor' class='bt-result'> $valor</button>";
-					}   
-				}else{echo "<p> Este livro não está registrado no banco de dados!</p>";}
-			}else{// caso seja autor
-				$q   = "SELECT titulo FROM arquivo_morto WHERE autor LIKE '%$autor%'";
-				$sel = executaQuery($con, $q);
-				$c   = mysqli_num_rows($sel);
-				if($c != 0){
-					echo "<p> O(s) livro(s) de $autor são: </p>";
-					$data = mysqli_fetch_all($sel, MYSQLI_ASSOC);
-					for($x = 0; $x < count($data); $x++){
-						$valor = $data[$x]['titulo'];
-						echo "<button type='submit' name='esc' value='$valor' class='bt-result'> $valor </button>";
-					}
-				}else{
-                    echo "<p> Este autor não está registrado no banco de dados </p>";
-                }
-			}
-            // Fechando a conexão
-            fechaDB($con);
-            ?>
-			</form>
-        </div>
-    </div>
-</body>
-</html>
+// Primeiro, preciso saber se o usuário está requisitando um titulo ou autor
+if(isset($_GET["titulo"])){
+  $titulo = $_GET["titulo"];
+  $titulo = strtoupper($titulo);
+  $q   = "SELECT idlivro_arq, titulo, autor FROM arquivo_morto WHERE titulo LIKE '%$titulo%'";
+  $sel = executaQuery($con, $q);
+  $c   = mysqli_num_rows($sel);
+  if($c != 0){
+    echo "<p>Selecione o livro abaixo para ver os dados dele:</p>";
+    $data = mysqli_fetch_all($sel, MYSQLI_ASSOC);
+    for($x = 0; $x < count($data); $x++){
+      $valor = $data[0]['titulo'] . " de " . $data[0]['autor'];
+      $idlivro = $data[0]['idlivro_arq'];
+      echo "<input type='radio' name='escolha' value='$idlivro' id='$idlivro' onchange='respondeChecked()'> <label for='$idlivro'> $valor </label> <br>";
+    }
+  } else {
+    echo "<p> Este livro não está registrado no banco de dados!</p>";
+  }
+} else {// caso seja autor
+  $autor = $_GET["autor"];
+  $autor = strtoupper($autor);
+  $q   = "SELECT titulo FROM arquivo_morto WHERE autor LIKE '%$autor%'";
+  $sel = executaQuery($con, $q);
+  $c   = mysqli_num_rows($sel);
+  if($c != 0){
+    echo "<p> O(s) livro(s) de $autor são: </p>";
+    $data = mysqli_fetch_all($sel, MYSQLI_ASSOC);
+    echo "<table>";
+    echo "<th> Título </th>";
+    echo "<th> Cod_livro</th>";
+    for($x = 0; $x < count($data); $x++){
+        $valor = $data[$x]['titulo'];
+        echo "<tr> <td> $valor </td> </tr>";
+    }
+    echo "</table>";
+    echo "<button type='button' onclick='window.print()' class='bt confirm'> Gerar Lista </button>";
+  } else {
+    echo "<p> O autor $autor não está registrado no banco de dados </p>";
+  }
+}
+// Fechando a conexão
+fechaDB($con);
+?>
