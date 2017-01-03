d94401ec15881d83b45b9f2d8914f0becb524389
Emanuel Gonçalves <emanuelgdesouza@gmail.com>
2017-01-03 10:03:52 -0200

    Update the delete book form

--- app/src/components/Forms/Book/DeleteBook/FormDeleteBook.vue
+++ app/src/components/Forms/Book/DeleteBook/FormDeleteBook.vue
@@ -2,33 +2,16 @@
   <div>
     <form>
       <label class="label"> Digite o título do livro para pesquisa </label>
-      <div class="control has-icon has-icon-right">
+      <div class="control has-icon has-icon-right has-addons">
         <input
           type="text"
           class="input is-medium is-expanded"
           :class="classSearchHelp"
-          placeholder="Título"
+          placeholder="Título para pesquisa"
           required
           autofocus
           v-model="search">
-          <i class="fa" :class="{ 'fa-check' : classSearchHelp === 'is-success' }"></i>
-          <span class="help" :class="classSearchHelp"> {{ searchHelp }} </span>
-      </div>
-      <hr>
-      <div class="columns">
-        <div class="column is-6">
-          <button
-            class="button is-success is-medium is-fullwidth"
-            type="button">
-            Confirmar
-          </button>
-        </div>
-        <div class="column is-6">
-          <input
-            class="button is-danger is-medium is-fullwidth"
-            type="reset"
-            value="Cancelar">
-        </div>
+          <button class="button is-medium is-success"> Pesquisar </button>
       </div>
     </form>
   </div>
@@ -39,7 +22,6 @@ export default {
   data () {
     return {
       search: '',
-      searchHelp: '',
       classSearchHelp: ''
     }
   },
@@ -47,13 +29,10 @@ export default {
     search () {
       const regExp = /0-9/
       if (this.search === '') {
-        this.searchHelp = ''
         this.classSearchHelp = ''
       } else if (regExp.test(this.search)) {
-        this.searchHelp = 'Título digitado incorretamente'
         this.classSearchHelp = 'is-danger'
       } else {
-        this.searchHelp = 'Título digitado corretamente'
         this.classSearchHelp = 'is-success'
       }
     }
