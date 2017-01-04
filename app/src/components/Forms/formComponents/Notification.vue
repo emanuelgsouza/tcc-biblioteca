14cb1da98cdd0e1dc2c2fedb1f27c7154411765c
Emanuel Gonçalves <emanuelgdesouza@gmail.com>
2017-01-04 15:44:50 -0200

    Insert notification component

---
+++ app/src/components/Forms/formComponents/Notification.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="notification is-danger">
+    <button class="delete" @click="closeNotification"></button>
+    Há um erro nos campos preenchidos, favor verificá-los!
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    closeNotification () {
+      this.$emit('closeNotification')
+    }
+  }
+}
+</script>
