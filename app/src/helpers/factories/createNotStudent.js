ae46227df8238f27dae1de8d1e466d1a7c9337d8
Emanuel Gonçalves <emanuelgdesouza@gmail.com>
2017-01-08 16:10:24 -0200

    Fixed bug in factories folder

--- app/src/helpers/factories/createNotStudent.js
+++ app/src/helpers/factories/createNotStudent.js
@@ -1,7 +1,12 @@
 import validation from '../validations'
+import { isNameValid } from '../validates'
 import { schemaNotStudent } from '../schemas'
 
 export default function (nome, endereco = null, telefone1, telefone2) {
+  if (!isNameValid(nome)) {
+    // Case invalid nome
+    return { err: true, msg: 'Digite um nome válido, somente letras e espaços' }
+  }
   if (!endereco) {
     const notStudent = { nome }
     return validation(schemaNotStudent, notStudent)
--- app/src/helpers/factories/createStudent.js
+++ app/src/helpers/factories/createStudent.js
@@ -1,7 +1,12 @@
 import validation from '../validations'
+import { isNameValid } from '../validates'
 import { schemaStudent } from '../schemas'
 
 export default function (nome, turma = 0) {
+  if (!isNameValid(nome)) {
+    // Case invalid nome
+    return { err: true, msg: 'Digite um nome válido, somente letras e espaços' }
+  }
   if (turma) {
     const student = { nome, turma }
     return validation(schemaStudent, student)
